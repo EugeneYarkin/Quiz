b9a92824cb88348380345b2bb6508bf9109dfcd8
EugeneYarkin <yarkin.eugene@gmail.com>
2022-04-30 17:45:31 +0500

    style Преобразовать кнопки НАЗАД и ДАЛЕЕ из Button в ImageButton

--- app/src/main/java/com/eyarkin/quiz/MainActivity.kt
+++ app/src/main/java/com/eyarkin/quiz/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import kotlin.math.abs
@@ -13,10 +14,10 @@ import kotlin.math.sqrt
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var backButton: Button
+    private lateinit var prevButton: ImageButton
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
+    private lateinit var nextButton: ImageButton
     private lateinit var questionTextView: TextView
     private val questionBank = listOf(
         Question(R.string.question_australia, true),
@@ -41,7 +42,7 @@ class MainActivity : AppCompatActivity() {
             R.string.incorrect_toast,
             Toast.LENGTH_SHORT
         )
-        backButton = findViewById(R.id.prev_button)
+        prevButton = findViewById(R.id.prev_button)
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
@@ -51,7 +52,7 @@ class MainActivity : AppCompatActivity() {
             currentIndex = (currentIndex + 1) % questionBank.size //интересно реализован цикл
             updateQuestion()
         }
-        backButton.setOnClickListener {view: View ->
+        prevButton.setOnClickListener {view: View ->
             currentIndex = currentIndex - 1
             if (currentIndex <0) {
                 currentIndex = questionBank.size-1
