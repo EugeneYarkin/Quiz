c7191776477b558c019275494bdb98cb0e94b07f
EugeneYarkin <yarkin.eugene@gmail.com>
2022-05-08 17:03:24 +0500

    feature Add ViewModel in MainActivity

--- app/src/main/java/com/eyarkin/quiz/MainActivity.kt
+++ app/src/main/java/com/eyarkin/quiz/MainActivity.kt
@@ -3,17 +3,23 @@ package com.eyarkin.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import kotlin.math.abs
 import kotlin.math.sqrt
+private const val TAG = "MainActivity"
 
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var prevButton: ImageButton
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
@@ -31,7 +37,15 @@ class MainActivity : AppCompatActivity() {
     private var currentBackIndex = questionBank.size
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
+
+        val provider: ViewModelProvider = ViewModelProviders.of(this)
+        val quizViewModel = provider.get(QuizViewModel::class.java)
+        Log.d(TAG, "Got a QuizViewModel: $quizViewModel")
+
+
         val toastCorrect = Toast.makeText(
             this,
             R.string.correct_toast,
@@ -71,6 +85,29 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    override fun onStart(){
+        super.onStart()
+        Log.d(TAG,"onStart() called")
+    }
+    override fun onResume(){
+        super.onResume()
+        Log.d(TAG,"onResume() called")
+    }
+    override fun onPause(){
+        super.onPause()
+        Log.d(TAG,"onPause called")
+    }
+    override fun onStop(){
+        super.onStop()
+        Log.d(TAG,"onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG,"onDestroy() called")
+    }
+
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
